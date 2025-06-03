71c8656ccbafa53158d70571a7bec825cc0a0105
Pedro Henrique <pedrogregolini@gmail.com>
2025-06-03 18:39:18 -0300

    gerando api Key do OpenWheather e ajustando regioes do leaf

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+
+const API_KEY = '14d0382101f97dbf642f275bdf705bd8';
+
+const bairros = [
+  { nome: "Capão Redondo", tipo_risco: "Deslizamento", lat: -23.6815, lon: -46.7903 },
+  { nome: "Jardim Ângela", tipo_risco: "Deslizamento", lat: -23.6736, lon: -46.7359 },
+  { nome: "Mooca", tipo_risco: "Alagamento", lat: -23.5614, lon: -46.5975 },
+  { nome: "Barra Funda", tipo_risco: "Alagamento", lat: -23.5254, lon: -46.6639 },
+  { nome: "Itaquera", tipo_risco: "Alagamento", lat: -23.5446, lon: -46.4695 },
+  { nome: "Pinheiros", tipo_risco: "Alagamento", lat: -23.5617, lon: -46.7011 },
+  { nome: "São Mateus", tipo_risco: "Deslizamento", lat: -23.5860, lon: -46.4282 },
+  { nome: "Vila Mariana", tipo_risco: "Alagamento", lat: -23.5894, lon: -46.6341 },
+  { nome: "Cidade Tiradentes", tipo_risco: "Deslizamento", lat: -23.5692, lon: -46.4108 },
+  { nome: "Lapa", tipo_risco: "Alagamento", lat: -23.5270, lon: -46.7059 }
+];
+
